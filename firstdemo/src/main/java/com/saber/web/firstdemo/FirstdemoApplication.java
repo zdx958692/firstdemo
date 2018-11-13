4ba56f0264d567d937df934098b04a8fb2cbd542
zdx958692 <3002691976@qq.com>
2018-11-13 17:13:43 +0800

    提交信息

---
+++ firstdemo/src/main/java/com/saber/web/firstdemo/FirstdemoApplication.java
@@ -0,0 +1,12 @@
+package com.saber.web.firstdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstdemoApplication.class, args);
+	}
+}
